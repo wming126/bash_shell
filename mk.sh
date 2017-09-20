7988e67208b83d635880a0abb92e4e3abaf07cc4
wming <wming126@126.com>
2017-09-20 13:38:12 +0800

    rm files

--- mk.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /opt/gcc-4.4.7-7215-n64-loongson/path
-make ARCH=mips CROSS_COMPILE=/opt/gcc-4.4.7-7215-n64-loongson/usr/bin/mips64el-redhat-linux- -j8
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#make clean
-#make mrproper
-cp arch/mips/configs/loongson3_defconfig .config
-#cp arch/mips/configs/loongson3a2000_defconfig .config
-make ARCH=mips menuconfig
-
